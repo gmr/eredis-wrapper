8f3e6e35af3678ebbe71e6863400d59ce0035e28
Gavin M. Roy <gavinmroy@gmail.com>
2014-06-22 23:12:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+RabbitMQ Wrapper for eredis
+===========================
+Clone this into rabbitmq-public-umbrella for use as a dependency by
+plugins that require it.
---
+++ package.mk
@@ -0,0 +1,14 @@
+APP_NAME:=eredis
+
+UPSTREAM_GIT:=https://github.com/wooga/eredis.git
+UPSTREAM_REVISION:=v1.0.6
+RETAIN_ORIGINAL_VERSION:=true
+
+INCLUDE_DIRS+=$(CLONE_DIR)
+
+ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/$(APP_NAME).app.src
+DO_NOT_GENERATE_APP_FILE=true
+
+define construct_app_commands
+	cp $(CLONE_DIR)/LICENSE $(APP_DIR)/LICENSE-MIT-eredis
+endef
